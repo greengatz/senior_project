276e42df590d37390a478568bd24daa62208ff1a
John Patterson <greengatz@yahoo.com>
2016-02-16 13:58:47 -0800

    Adding solvers to repo

---
+++ senior project/python solvers/2048/SearchSolver.py
@@ -0,0 +1,111 @@
+'''
+Created on Feb 1, 2016
+
+@author: John_2
+'''
+from _overlapped import NULL
+from GameState import GameState
+from random import randint
+from Directions import *
+
+class SearchSolver(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, inDepth):
+        '''
+        Constructor
+        '''
+        self.depth = inDepth
+        self.game = GameState()
+        self.numMoves = 0
+        pass
+    
+    
+    def playGame(self):
+        while (self.game.isGoing()):
+            self.startSearch(self.game.copyArr(), self.depth)
+            break
+        pass
+    
+    
+    # determines the move with the highest ev with the given search depth
+    def startSearch(self, board, depth):
+        bestOption = NULL
+        bestVal = -1
+        
+        # base case, depth == 0
+        # in this case, estimate rest of the way greedy?
+        if (depth == 0):
+            return (Move.up, 0)
+        
+        upVal = self.estimateValue(depth - 1, board, Move.up)
+        print(upVal)
+        if (upVal > bestVal):
+            bestOption = Move.up
+        
+        rightVal = self.estimateValue(depth - 1, board, Move.right)
+        if (rightVal > bestVal):
+            bestOption = Move.right
+        
+        return (bestOption, bestVal)
+    
+    
+    def estimateValue(self, depth, board, move):
+        ev2 = [[0 for x in range(4)] for x in range(4)]
+        ev4 = [[0 for x in range(4)] for x in range(4)]
+        ev = 0
+        numOptions = 0
+        
+        # go through all options
+        if(self.game.isValid(move)): 
+            baseGameUp = GameState()
+            baseGameUp.setBoard(board)
+            baseGameUp.executeMove(move)
+            
+            for x in range (0, 4):
+                for y in range (0, 4):
+                    if(baseGameUp.gameArray[x][y] == 0):
+                        numOptions += 2
+                        
+                        # per cell ev expecting 4
+                        pretendBoard2 = baseGameUp.copyArr()
+                        pretendGame2 = GameState()
+                        pretendBoard2[x][y] = 2
+                        pretendGame2.setBoard(pretendBoard2)
+                        searchEv = self.startSearch(pretendGame2.copyArr(), depth - 1)
+                        ev2[x][y] = searchEv[1]
+                        pretendGame4 = NULL
+                    
+                        # per cell ev expecting 4
+                        pretendBoard4 = baseGameUp.copyArr()
+                        pretendGame4 = GameState()
+                        pretendBoard4[x][y] = 4
+                        pretendGame4.setBoard(pretendBoard4)
+                        searchEv = self.startSearch(pretendGame4.copyArr(), depth - 1)
+                        ev4[x][y] = searchEv[1]
+                        pretendGame4 = NULL
+                    
+                        pass
+            
+            # TODO adjust per cell for likelihood
+            
+            # TODO average the per cell evs together to find move ev
+
+        return ev
+    
+    
+    def getScore(self):
+        return self.game.getScore()
+    
+    def getMaxTile(self):
+        return self.game.getMaxTile()
+    
+    def getMoves(self):
+        return self.numMoves
+    
+    def printGame(self):
+        self.game.printState()
+        pass
---
+++ senior project/python solvers/2048/mainDriver.py
@@ -0,0 +1,121 @@
+'''
+Created on Jan 11, 2016
+
+@author: John_2
+'''
+
+from RandomSolver import RandomSolver
+from GreedySolver import GreedySolver
+from GreedySearch import GreedySearch
+from SearchSolver import SearchSolver
+from Directions import *
+from GameState import GameState
+from _overlapped import NULL
+
+toRun = 1;
+searchDepth = 2;
+
+def playRandom(numTrials):
+    print("running " + str(numTrials) + " random games")
+    totalScore = 0
+    maxTile = 0
+    
+    for x in range(0, numTrials):
+        #print("game: " + str(x))
+        solver = RandomSolver()
+        solver.playGame()
+        totalScore += solver.getScore()
+        if(solver.getMaxTile() > maxTile):
+            maxTile = solver.getMaxTile()
+        solver = NULL
+    
+    print("average score: " + str(totalScore / numTrials))
+    print("max tile: " + str(maxTile))
+    pass
+
+
+def playGreedy(numTrials):
+    print("running " + str(numTrials) + " greedy games")
+    totalScore = 0
+    maxTile = 0
+    
+    for x in range(0, numTrials):
+        #print("game: " + str(x))
+        solver = GreedySolver()
+        solver.playGame()
+        totalScore += solver.getScore()
+        if(solver.getMaxTile() > maxTile):
+            maxTile = solver.getMaxTile()
+        solver = NULL
+    
+    print("average score: " + str(totalScore / numTrials))
+    print("max tile: " + str(maxTile))
+    pass
+
+
+def playGreedySearch(numTrials, depth=searchDepth):
+    print("running " + str(numTrials) + " greedy search games with depth " + str(depth))
+    totalScore = 0
+    maxTile = 0
+    
+    for x in range(0, numTrials):
+        #print("game: " + str(x))
+        solver = GreedySearch(depth)
+        solver.playGame()
+        totalScore += solver.getScore()
+        if(solver.getMaxTile() > maxTile):
+            maxTile = solver.getMaxTile()
+        solver = NULL
+    
+    print("average score: " + str(totalScore / numTrials))
+    print("max tile: " + str(maxTile))
+    pass
+
+
+def playSearch(numTrials):
+    print("running " + str(numTrials) + " searched games")
+    totalScore = 0
+    maxTile = 0
+    
+    for x in range(0, numTrials):
+        #print("game: " + str(x))
+        solver = SearchSolver(searchDepth)
+        solver.playGame()
+        totalScore += solver.getScore()
+        if(solver.getMaxTile() > maxTile):
+            maxTile = solver.getMaxTile()
+        solver = NULL
+    
+    print("average score: " + str(totalScore / numTrials))
+    print("max tile: " + str(maxTile))
+    pass
+
+
+def playHuman():
+    print("human is playing the game")
+    aGame = GameState();
+    
+    'game loop'
+    while aGame.isGoing():
+        aGame.printState()
+        entered = input("enter a move")
+        key = entered[0]
+        if key == 'q':
+            break
+        elif key == 'w':
+            aGame.takeMove(Move.up)
+        elif key == 'a':
+            aGame.takeMove(Move.left)
+        elif key == 's':
+            aGame.takeMove(Move.down)
+        elif key == 'd':
+            aGame.takeMove(Move.right)
+    
+    print("game over")
+    aGame.printState()
+    pass
+
+
+if __name__ == '__main__':
+    playGreedySearch(toRun)
+    pass
